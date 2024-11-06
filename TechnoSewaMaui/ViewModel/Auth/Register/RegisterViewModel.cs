35eaf0c1550b5dbe5fc3c604da4086ac6f38fb63
Shirish-Bashyal <104261483+Shirish-Bashyal@users.noreply.github.com>
2024-11-06 19:19:19 +0545

    register page ui initiated

--- TechnoSewaMaui/ViewModel/Auth/Register/RegisterViewModel.cs
+++ TechnoSewaMaui/ViewModel/Auth/Register/RegisterViewModel.cs
@@ -3,9 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using TechnoSewaMaui.ViewModel.Base;
 
 namespace TechnoSewaMaui.ViewModel.Auth.Register
 {
-    public partial class RegisterViewModel : BaseViewModel { }
+    [QueryProperty(nameof(PhoneNumber), "phoneNumber")]
+    public partial class RegisterViewModel : BaseViewModel
+    {
+        private string _phoneNumber;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                _phoneNumber = value;
+                OnPropertyChanged(nameof(PhoneNumber));
+            }
+        }
+
+        [ObservableProperty]
+        private string _fullName;
+
+        [ObservableProperty]
+        private string _email;
+
+        [ObservableProperty]
+        private string _password;
+
+        [ObservableProperty]
+        private string _confirmPassword;
+
+        [ObservableProperty]
+        private string _city;
+
+        [ObservableProperty]
+        private string _wardNo;
+
+        [ObservableProperty]
+        private string _toleName;
+
+        public RegisterViewModel() { }
+
+        [RelayCommand]
+        public async Task RegisterButtonCLicked()
+        {
+            await Shell.Current.DisplayAlert("Success", "It is clicked", "OK");
+        }
+    }
 }
