20635e5bef62e89ffa7b4ef724fd51becefdb70a
Shirish-Bashyal <104261483+Shirish-Bashyal@users.noreply.github.com>
2024-12-17 08:31:02 +0545

    consumer profile get api completed

---
+++ Application/Interfaces/User/Consumer/IProfileService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Response;
+
+namespace Application.Interfaces.User.Consumer
+{
+    public interface IProfileService
+    {
+        Task<ServiceResponse<object>> ConsumerProfile(string userId);
+
+        //Task<ServiceResponse<object>> EditProfile();
+    }
+}
---
+++ TechnoSewa/Controllers/ConsumerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using Application.Interfaces.User.Consumer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechnoSewa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsumerController : ControllerBase
+    {
+        private readonly IProfileService _profile;
+
+        public ConsumerController(IProfileService profile)
+        {
+            _profile = profile;
+        }
+
+        [HttpGet("profile")]
+        [Authorize]
+        public async Task<IActionResult> GetConsumerProfile()
+        {
+            string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (userId != null)
+            {
+                var profile = await _profile.ConsumerProfile(userId);
+                return Ok(profile);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
