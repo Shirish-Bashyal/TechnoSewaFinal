bd9d65991c2a86bd22ccf443b6b20c535c0ed6f1
chetanachaudhary <chetanachaudhary445@gmail.com>
2025-06-13 14:43:55 +0545

    Technician home page ui components adding

--- app/auth/tech.tsx
+++ app/auth/tech.tsx
@@ -5,6 +5,12 @@ import { useRouter } from "expo-router";
 
 const Tech = () => {
   const router = useRouter();
+   const handleTechnician = () => {
+    router.push("/Technician/(root)/(tabs)");
+  };
+  const handleClient = () => {
+    router.push("/(root)/(tabs)");
+  };
   return (
     <SafeAreaView className="h-full bg-gray-100">
         <View className="flex justify-center items-center flex-1  ">
@@ -15,14 +21,14 @@ const Tech = () => {
         </Text>
       </View>
       <View className="mb-1 flex flex-row gap-5">
-        <TouchableOpacity className="bg-[#7A4DFF]/[1.6] shadow-md shadow-zinc-300 rounded-lg w-32 h-14 flex justify-center items-center  mt-8 mx-2 ">
+        <TouchableOpacity onPress={handleClient} className="bg-[#7A4DFF]/[1.6] shadow-md shadow-zinc-300 rounded-lg w-32 h-14 flex justify-center items-center  mt-8 mx-2 ">
           <View className="flex flex-row items-center justify-center">
             <Text className="text-lg font-rubik-bold text-white text-center mt-2 ">
               Client
             </Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity className="bg-[#7A4DFF]/[1.6] shadow-md shadow-zinc-300 rounded-lg w-32 h-14 flex justify-center items-center mt-8 mx-2 ">
+        <TouchableOpacity onPress={handleTechnician} className="bg-[#7A4DFF]/[1.6] shadow-md shadow-zinc-300 rounded-lg w-32 h-14 flex justify-center items-center mt-8 mx-2 ">
           <View className="flex flex-row items-center justify-center">
             <Text className="text-lg font-rubik-bold text-white text-center mt-2 ">
               Technician
